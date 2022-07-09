6c737be1e9bae8dd6bfe00a2d35751c12d99b124
shellrow <81893184+shellrow@users.noreply.github.com>
2022-07-10 03:08:35 +0900

    Update option.rs

--- nodemap-core/src/option.rs
+++ nodemap-core/src/option.rs
@@ -1,5 +1,5 @@
 use std::time::Duration;
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum ExecType {
@@ -77,4 +77,37 @@ pub struct ScanOption {
     pub request_method: String,
     pub data_provider: String,
     pub save_file_path: String,
+}
+
+impl ScanOption {
+    pub fn new() -> ScanOption {
+        ScanOption{
+            exec_type: ExecType::PortScan,
+            interface_index: u16::MIN,
+            interface_name: String::new(),
+            src_ip: IpAddr::V4(Ipv4Addr::LOCALHOST),
+            src_port: u16::MIN,
+            targets: vec![],
+            protocol: String::new(),
+            max_hop: 64,
+            host_scan_type: netscan::setting::ScanType::IcmpPingScan,
+            port_scan_type: netscan::setting::ScanType::TcpConnectScan,
+            ping_type: tracert::protocol::Protocol::Icmpv4,
+            timeout: Duration::from_millis(30000),
+            wait_time: Duration::from_millis(200),
+            send_rate: Duration::from_millis(0),
+            default_scan: true,
+            service_detection: false,
+            os_detection: false,
+            async_scan: true,
+            use_wordlist: false,
+            use_content: false,
+            accept_invalid_certs: false,
+            wordlist_path: String::new(),
+            content_path: String::new(),
+            request_method: String::new(),
+            data_provider: String::new(),
+            save_file_path: String::new(),
+        }
+    }
 }
