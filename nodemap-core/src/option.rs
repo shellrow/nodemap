29fbd06a63420d5acfde6d76ca9cdd071e30a2c0
shellrow <81893184+shellrow@users.noreply.github.com>
2022-07-09 23:14:36 +0900

    Update option.rs

--- nodemap-core/src/option.rs
+++ nodemap-core/src/option.rs
@@ -2,8 +2,9 @@ use netscan::setting::ScanType;
 use std::time::Duration;
 use std::fs::read_to_string;
 use std::net::{IpAddr, Ipv4Addr};
-use ipnet::{Ipv4Net};
+use ipnet::Ipv4Net;
 
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum ExecType {
     PortScan,
     HostScan,
@@ -15,12 +16,41 @@ pub enum ExecType {
     PassiveScan
 }
 
+impl ExecType {
+    pub fn name(&self) -> String {
+        match *self {
+            ExecType::PortScan => String::from("Port scan"),
+            ExecType::HostScan => String::from("Host scan"),
+            ExecType::Ping => String::from("Ping"),
+            ExecType::Traceroute => String::from("Traceroute"),
+            ExecType::UriScan => String::from("URI scan"),
+            ExecType::DomainScan => String::from("Domain scan"),
+            ExecType::BatchScan => String::from("Batch scan"),
+            ExecType::PassiveScan => String::from("Passive scan"),
+        }
+    }
+    pub fn description(&self) -> String {
+        match *self {
+            ExecType::PortScan => String::from("Port scan"),
+            ExecType::HostScan => String::from("Host scan"),
+            ExecType::Ping => String::from("Ping"),
+            ExecType::Traceroute => String::from("Traceroute"),
+            ExecType::UriScan => String::from("URI scan"),
+            ExecType::DomainScan => String::from("Domain scan"),
+            ExecType::BatchScan => String::from("Batch scan"),
+            ExecType::PassiveScan => String::from("Passive scan"),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct TargetInfo {
     pub ip_addr: IpAddr,
     pub host_name: String,
     pub ports: Vec<u16>,
 }
 
+#[derive(Clone, Debug)]
 pub struct ScanOption {
     pub exec_type: ExecType,
     pub interface_index: u16,
@@ -30,6 +60,9 @@ pub struct ScanOption {
     pub targets: Vec<TargetInfo>,
     pub protocol: String,
     pub max_hop: u8,
+    pub host_scan_type: netscan::setting::ScanType,
+    pub port_scan_type: netscan::setting::ScanType,
+    pub ping_type: tracert::protocol::Protocol,
     pub timeout: Duration,
     pub wait_time: Duration,
     pub send_rate: Duration,
