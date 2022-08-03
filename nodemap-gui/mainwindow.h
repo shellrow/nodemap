6a3ec9fd7358b69f0fff261fdf83dd62dca92f0c
shellrow <81893184+shellrow@users.noreply.github.com>
2022-08-03 23:26:59 +0900

    Add dark theme

--- nodemap-gui/mainwindow.h
+++ nodemap-gui/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStyleFactory>
+#include <qdebug.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +16,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void applyDarkStyle();
 
 private:
     Ui::MainWindow *ui;
