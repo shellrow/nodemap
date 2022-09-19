382c8751665a027c009546de44c7b7b1b7db21dc
shellrow <81893184+shellrow@users.noreply.github.com>
2022-09-19 16:34:17 +0900

    Update define.rs

--- core/src/define.rs
+++ core/src/define.rs
@@ -1,3 +1,4 @@
+// MPSC(Multi Producer, Single Consumer) FIFO queue communication messages
 pub const MESSAGE_START_PORTSCAN: &str = "START_PORTSCAN";
 pub const MESSAGE_END_PORTSCAN: &str = "END_PORTSCAN";
 pub const MESSAGE_START_SERVICEDETECTION: &str = "START_SERVICEDETECTION";
@@ -8,3 +9,7 @@ pub const MESSAGE_START_HOSTSCAN: &str = "START_HOSTSCAN";
 pub const MESSAGE_END_HOSTSCAN: &str = "END_HOSTSCAN";
 pub const MESSAGE_START_ARPSCAN: &str = "START_ARPSCAN";
 pub const MESSAGE_END_ARPSCAN: &str = "END_ARPSCAN";
+
+// Passive Scan URL
+pub const URL_SHODAN: &str = "https://internetdb.shodan.io/";
+pub const URL_CRT: &str = "https://crt.sh/";
