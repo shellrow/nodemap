b9f0b9962b2eea64dfebf8ac59cc8f5076b7209f
shellrow <81893184+shellrow@users.noreply.github.com>
2022-10-18 20:57:58 +0900

    Update define.rs

--- cli/src/define.rs
+++ cli/src/define.rs
@@ -1,4 +1,4 @@
-pub const CRATE_UPDATE_DATE: &str = "2022-10-16";
+pub const CRATE_UPDATE_DATE: &str = "2022-10-18";
 pub const CRATE_REPOSITORY: &str = "https://github.com/shellrow/nodemap";
 pub const DEFAULT_SRC_PORT: u16 = 53443;
 pub const NODEMAP_OUI: &str = include_str!("../data/nodemap-oui.json");
@@ -6,6 +6,5 @@ pub const NODEMAP_TCP_PORT: &str = include_str!("../data/nodemap-tcp-port.json")
 pub const NODEMAP_DEFAULT_PORTS: &str = include_str!("../data/nodemap-default-ports.txt");
 pub const NODEMAP_HTTP: &str = include_str!("../data/nodemap-http.txt");
 pub const NODEMAP_HTTPS: &str = include_str!("../data/nodemap-https.txt");
-//pub const NODEMAP_OS: &str = include_str!("../data/nodemap-os-simple.json");
 pub const NODEMAP_OSDB: &str = include_str!("../data/nodemap-osdb.json");
 pub const NODEMAP_OS_TTL: &str = include_str!("../data/nodemap-os-ttl.json");
