a153ee712e75d5432da45245a21e9dbe951aa714
shellrow <81893184+shellrow@users.noreply.github.com>
2022-10-22 11:37:51 +0900

    Add modules

--- gui/src/main.js
+++ gui/src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './index.css';
+import VNetworkGraph from "v-network-graph";
+import "v-network-graph/lib/style.css";
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/dark/css-vars.css';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(VNetworkGraph).use(ElementPlus).mount('#app');
