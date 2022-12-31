f0a9073c19942a39730dd656718e82364995bc7e
shellrow <81893184+shellrow@users.noreply.github.com>
2022-12-31 21:27:00 +0900

    Create define.js

---
+++ gui/src/define.js
@@ -0,0 +1,17 @@
+// Const Define for nodemap-desktop frontend
+// Port Option
+const PORT_OPTION_DEFAULT = "default";
+const PORT_OPTION_WELL_KNOWN = "well_known";
+const PORT_OPTION_CUSTOM_LIST = "custom_list";
+
+// Scan Type
+const PORTSCAN_TYPE_TCP_SYN = "tcp_syn_scan";
+const PORTSCAN_TYPE_TCP_CONNECT = "tcp_connect_scan";
+
+export { 
+    PORT_OPTION_DEFAULT, 
+    PORT_OPTION_WELL_KNOWN, 
+    PORT_OPTION_CUSTOM_LIST,
+    PORTSCAN_TYPE_TCP_SYN,
+    PORTSCAN_TYPE_TCP_CONNECT,
+};
